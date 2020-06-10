b1c00d6456cd0fca927275f0975f762439afecff
Neil W <neilstats@users.noreply.github.com>
2020-06-10 20:52:01 +0100

    update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(...) {
- packageStartupMessage("The method for setting aesthetics in make_forest_plot has changed. Please see the help and vignettes for details.")
+ packageStartupMessage("The method for setting aesthetics in make_forest_plot has changed.\nPlease see the help and vignettes for details.")
 }
