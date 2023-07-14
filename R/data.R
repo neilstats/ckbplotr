9fc2632843fd2d4eaac9a6edf391bec84faf6c69
neilstats <neilstats@users.noreply.github.com>
2023-07-14 22:01:06 +0100

    remove example data

--- R/data.R
+++
@@ -1,8 +0,0 @@
-#' Synthetic data for demoing shape_plot
-"ckbplotr_shape_data"
-
-#' Synthetic data for demoing forest_plot
-"ckbplotr_forest_data"
-
-#' Row labels data for demoing shape_plot
-"ckbplotr_row_labels"
