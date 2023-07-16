953b60560c438dc7cc0ddcc6cb524663a1db8a70
neilstats <neilstats@users.noreply.github.com>
2023-07-16 10:59:25 +0100

    change plot_like_ckb() to use ckb_style()

--- R/ckb-style.R
+++ R/ckb-style.R
@@ -38,154 +38,10 @@ theme_ckb <- function(base_size      = 11,
 
 
 
-#' Make a ggplot into CKB style
-#'
-#' @inheritParams theme_ckb
-#' @param plot A ggplot2 plot object.
-#' @param xlims A numeric vector of length two. The limits of the x-axis.
-#' @param ylims A numeric vector of length two. The limits of the y-axis.
-#' @param gap A numeric vector of length two. The gap between plotting area and axis to the left and bottom of the plot, as a proportion of the x-axis length. (Default: c(0.025, 0.025))
-#' @param ext A numeric vector of length two. The extensions to add to the right and top of the plot, as a proportion of the x-axis length. (Default: c(0, 0))
-#' @param ratio The ratio (y-axis:x-axis) to use for the plot. Ignored if both width and height are set. (Default: 1.5)
-#' @param width A `grid::unit` object to set the width of the plot (not including the gap or extension).
-#' @param height A `grid::unit` object to set the height of the plot (not including the gap or extension).
-#' @param colour Colour for non-data aspects of the plot. (Default: "black")
-#' @param axes Choice of axis lines to add to the plot, one of "both", "x" or "y". (Default: "both")
-#'
-#' @return A ggplot2 plot.
-#'
-#' @import ggplot2
-#' @export
-
-plot_like_ckb <- function(
-    plot,
-    xlims=NULL,
-    ylims=NULL,
-    gap=c(0.025,0.025),
-    ext=c(0,0),
-    ratio=1.5,
-    width = NULL,
-    height = NULL,
-    base_size = 11,
-    base_line_size = base_size/22,
-    colour = "black",
-    plot.margin = margin(0.5, 1.5, 0.5, 0.5, "lines"),
-    axes = "both"
-){
-
-  # check arguments
-  if (!axes %in% c("both", "x", "y", "none")){rlang::abort("axes should be one of 'both', 'x', 'y' or 'none'.")}
-
-  # panel sizes
-  if (missing(width) & missing(height)){
-    full_width <- 1
-    full_height <- (ratio + gap[[2]] + ext[[2]]) / (1 + gap[[1]] + ext[[1]])
-  } else if (!missing(width) & missing(height)){
-    full_width <- width * (1 + gap[[1]] + ext[[1]])
-    full_height <-  width * (ratio + gap[[2]] + ext[[2]])
-  } else if (missing(width) & !missing(height)){
-    full_width <- height / ratio * (1 + gap[[1]] + ext[[1]])
-    full_height <- height / ratio * (ratio + gap[[2]] + ext[[2]])
-  } else if (!missing(width) & !missing(height)){
-    full_width <- width * (1 + gap[[1]] + ext[[1]])
-    full_height <- height + width * (gap[[2]] + ext[[2]])
-    ratio <- as.numeric(grid::convertUnit(height, "mm")) / as.numeric(grid::convertUnit(width, "mm"))
-  }
-
-  # get plot axis transformations
-  tf_x    <- ggplot_build(plot)$layout$panel_scales_x[[1]]$trans$transform
-  invtf_x <- ggplot_build(plot)$layout$panel_scales_x[[1]]$trans$inverse
-  tf_y    <- ggplot_build(plot)$layout$panel_scales_y[[1]]$trans$transform
-  invtf_y <- ggplot_build(plot)$layout$panel_scales_y[[1]]$trans$inverse
-  tf_x    <- ifelse(is.null(tf_x), identity, tf_x)
-  invtf_x <- ifelse(is.null(invtf_x), identity, invtf_x)
-  tf_y    <- ifelse(is.null(tf_y), identity, tf_y)
-  invtf_y <- ifelse(is.null(invtf_y), identity, invtf_y)
-
-  # if xlims or ylims not given as argument, get axis limits from range in ggplot plot
-  if (is.null(xlims)){
-    if (!is.null(ggplot_build(plot)$layout$panel_scales_x[[1]]$limits)){
-      xlims <- ggplot_build(plot)$layout$panel_scales_x[[1]]$limits
-    } else {
-      xlims <- ggplot_build(plot)$layout$panel_scales_x[[1]]$range$range
-    }
-    xlims <- range(pretty(xlims))
-  }
-
-  if (is.null(ylims)){
-    if (!is.null(ggplot_build(plot)$layout$panel_scales_y[[1]]$limits)){
-      ylims <- ggplot_build(plot)$layout$panel_scales_y[[1]]$limits
-    } else {
-      ylims <- ggplot_build(plot)$layout$panel_scales_y[[1]]$range$range
-    }
-    ylims <- range(pretty(ylims))
-  }
-
-  # calculate plot limits
-  limits <- list(xaxis = xlims, yaxis = ylims)
-
-  ## check for infinite values in transformed axis limits
-  if (any(!is.finite(tf_x(limits[["xaxis"]])))) {
-    rlang::abort("Infinite or NaN values in x-axis. Provide axis limits and check transformation of x scale.")
-  }
-  if (any(!is.finite(tf_y(limits[["yaxis"]])))) {
-    rlang::abort("Infinite or NaN values in y-axis. Provide axis limits and check transformation of y scale.")
-  }
-
-  addtox <- c(gap[[1]]*diff(range(tf_x(limits[["xaxis"]]))),
-              ext[[1]]*diff(range(tf_x(limits[["xaxis"]]))))
-  addtoy <- c((1/ratio)*gap[[2]]*diff(range(tf_y(limits[["yaxis"]]))),
-              (1/ratio)*ext[[2]]*diff(range(tf_y(limits[["yaxis"]]))))
-  limits[["x"]] <- invtf_x(tf_x(limits[["xaxis"]]) + c(-1, 1)*addtox)
-  limits[["y"]] <- invtf_y(tf_y(limits[["yaxis"]]) + c(-1, 1)*addtoy)
-
-  # update plot
-  plot <- plot +
-    coord_cartesian(xlim   = limits[["x"]],
-                    ylim   = limits[["y"]],
-                    expand = FALSE,
-                    clip = "off") +
-    ggh4x::force_panelsizes(rows = full_height,
-                            cols = full_width,
-                            respect = TRUE) +
-    theme_ckb(base_size = base_size,
-              base_line_size = base_line_size,
-              colour = colour,
-              plot.margin = plot.margin)
-
-  # add axis lines to plot
-  if (axes %in% c("both", "y")){
-    plot <- plot +
-      annotate(geom = "segment",
-               x    = limits[["x"]][[1]],
-               xend = limits[["x"]][[1]],
-               y    = limits[["yaxis"]][[1]],
-               yend = limits[["yaxis"]][[2]],
-               linewidth  = base_line_size,
-               lineend = "round",
-               colour = colour)
-  }
-
-  if (axes %in% c("both", "x")){
-    plot <- plot +
-      annotate(geom = "segment",
-               x    = limits[["xaxis"]][[1]],
-               xend = limits[["xaxis"]][[2]],
-               y    = limits[["y"]][[1]],
-               yend = limits[["y"]][[1]],
-               linewidth  = base_line_size,
-               lineend = "round",
-               colour = colour)
-  }
-
-  return(plot)
-}
-
 
 
 #' Make a ggplot into CKB style
 #'
-#' `r lifecycle::badge("experimental")`
 #'
 #' @inheritParams theme_ckb
 #' @param xlims A numeric vector of length two. The limits of the x-axis.
@@ -358,3 +214,24 @@ ggplot_add.ckbplot <- function(object, plot, object_name) {
 
   return(plot)
 }
+
+
+
+
+
+
+#' Make a ggplot into CKB style
+#'
+#' @param plot A ggplot2 plot
+#' @param ... Arguments passed to ckb_style()
+#'
+#' @return A ggplot2 plot.
+#'
+#' @import ggplot2
+#' @export
+
+plot_like_ckb <- function(plot, ...){
+  plot <- plot + ckb_style(...)
+  return(plot)
+}
+
