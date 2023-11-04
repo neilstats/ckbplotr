2bbd39b7678ce7b6b1e9c72811998ae95a7b1291
neilstats <neilstats@users.noreply.github.com>
2023-11-04 17:21:28 +0000

    change package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(...) {
   packageStartupMessage(
     cli::format_inline(
-      "This version of the package introduces many changes.\nPlease see {.href [https://neilstats.github.io/ckbplotr/news](https://neilstats.github.io/ckbplotr/news)} for details."
+      "See {.href [https://neilstats.github.io/ckbplotr](https://neilstats.github.io/ckbplotr)} for details on using this package."
     )
   )
 }
